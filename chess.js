6695ecd2770b207e305c4af84f458d7c9369ec3f
Aaron Marriner <amarriner@amarriner.com>
2014-03-03 19:42:35 +0000

    Cause the bot to favorite wins, accommodate for promotions correctly

--- chess.js
+++ chess.js
@@ -7,6 +7,11 @@ var k       = new Keys();
 // Twitter function to post images
 var twitter_update_with_media = require('twitter_update_with_media');
 
+// Alternate NPM module for more twitter functionality. Should probably
+// figure out a better way to do this other than having two separate
+// twitter libraries
+var Twit    = require('twit');
+
 // Required to build the board image using GD
 // https://www.npmjs.org/package/node-gd
 var gd      = require('node-gd');
@@ -44,6 +49,12 @@ chess['w'] = {
                     consumer_secret : k.twitter_consumer_secret,
                     token           : k.white_twitter_access_token,
                     token_secret    : k.white_twitter_access_token_secret
+             }),
+   twit    : new Twit({
+                    consumer_key        : k.twitter_consumer_key,
+                    consumer_secret     : k.twitter_consumer_secret,
+                    access_token        : k.white_twitter_access_token,
+                    access_token_secret : k.white_twitter_access_token_secret
              })
 };
 
@@ -56,6 +67,12 @@ chess['b'] = {
                     consumer_secret : k.twitter_consumer_secret,
                     token           : k.black_twitter_access_token,
                     token_secret    : k.black_twitter_access_token_secret
+             }),
+   twit    : new Twit({
+                    consumer_key        : k.twitter_consumer_key,
+                    consumer_secret     : k.twitter_consumer_secret,
+                    access_token        : k.black_twitter_access_token,
+                    access_token_secret : k.black_twitter_access_token_secret
              })
 };
 
@@ -76,9 +93,9 @@ function process_next_move() {
    // http://wbec-ridderkerk.nl/html/UCIProtocol.html
    nexpect.spawn(player.engine, options={verbose: false})
           .sendline('uci')
-          .sendline('set ownbook true')             // Use engine's internal opening book
+          .sendline('set ownbook true')               // Use engine's internal opening book
           .sendline('ucinewgame')
-          .sendline('position fen ' + position)     // Moved the engine's board the current position
+          .sendline('position fen ' + position)       // Moved the engine's board the current position
           .sendline('go depth 20 movetime 300000')    // Stronger depth search, but limit to 5 mintes
           .wait('bestmove')
           .sendline('quit')
@@ -94,7 +111,10 @@ function process_next_move() {
                 // seems to be troublesome with varying engines as they're not standardized
                 var move = '';
 
+                // Looping through the last few lines of output to attempt to find the move the engine decided on
                 for (var z = 1; z <= 5; z++) {
+
+                   // Stripping out extraneous text in output
                    move = output[output.length - z].replace('bestmove ', '');
 
                    if (move.indexOf(' ponder') >= 0)
@@ -103,13 +123,21 @@ function process_next_move() {
                    if (move.indexOf(' ') >= 0)
                       move = move.split(' ')[0];
 
+                   // If move is more than four characters long then it's a promotion, strip the last character
+                   var promotion = '';
+                   if (move.length > 4) {
+                      promotion = move.substring(4, 5);
+                      move = move.substring(0, 4);
+                   }
+
                    // If we found a "valid" move from the engines STDOUT
                    if (move) {
 
                       // Attempt the move against the Chess.JS object, erroring off if there was a problem
-                      if (! c.move({ from: move.substr(0, 2), to: move.substring(2, 4) })) {
+                      if (! c.move({ from: move.substr(0, 2), to: move.substring(2, 4), promotion: promotion })) {
                          interval = 10000;
-                         console.log('*** ERROR MOVING!!! *** (' + move + ' - ' + output[output.length - z] + ')');
+                         console.log('*** ERROR MOVING!!! *** (' + move + ' - ' + promotion
+                                                                        + ' - ' + output[output.length - z] + ')');
                       }
 
                       // Otherwise, build a new PNG of the board, save the FEN and PGN output to file,
@@ -135,9 +163,19 @@ function process_next_move() {
                          // Tweet move and board
                          var tweet = '@ChessBot' + ((player.color == 'White') ? 'Black' : 'White') + ' ' +  
                                      turn + '. ' + player.move + last_move + ' ' + hashtag + ' ';
-                         player.twitter.post(tweet, 'images/board.png', function(err, response) {
+                         player.twitter.post(tweet, 'images/board.png', function(err, response, body) {
                             if (err)
-                               console.log(err);
+                               console.log('*** ERROR TWEETING ***: ' + err);
+
+                            // If this tweet resulted in checkmate, favorite it
+                            if (c.in_checkmate()) {
+                               var tw = (c.turn() == 'w') ? 'b' : 'w';
+
+                               chess[tw].twit.post('favorites/create', {id: JSON.parse(body).id_str}, function(err, reply) {
+                                  if (err)
+                                     console.log('*** ERROR FAVORITING ***: ' + err);
+                               });
+                            }
                          });
 
                          // Debug console output
